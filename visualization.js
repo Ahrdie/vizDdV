69a574d0fd962786bc6b10f765c5c9f8fa2d6b40
Robert Deppe <robert.deppe@posteo.de>
2019-01-17 00:06:12 +0100

    [Add] parsing of receive dates - Also added parsing of various bools

--- visualization.js
+++ visualization.js
@@ -11,35 +11,63 @@ function drawData(){
 
     var ssv = d3.dsvFormat(";");                          // ssv := semicolon seperated values
     var parseBirthday =     d3.timeParse("%d.%m.%Y");
-    var parseLetterSendDate = d3.timeParse("%m/%d/%y");
+    var parseSlashDate = d3.timeParse("%m/%d/%y");
     var parseMailSendTime = d3.timeParse("%d-%B-%Y-%H:%M");
+    var parseReceiveDate = d3.timeParse("%m/%d/%y-%H:%M");
+    var parseReceiveDateFullYear = d3.timeParse("%m/%d/%Y-%H:%M");
 
     function parseSendDate(letter, mailSendDate, sendTime, letterSendDate){
+      if(letter == 0) {
+        return parseMailSendTime(mailSendDate + "-2018-" + sendTime);
 
-        if(letter == 0) {
-            var t = parseMailSendTime(mailSendDate + "-2018-" + sendTime)
-            return t;
+      } else if (letter == 1) {
+        return parseSlashDate(letterSendDate);
 
-        } else if (letter == 1) {
-          return parseLetterSendDate(letterSendDate);
-        } else {
-          return null
-        }
+      } else {
+        return null
+      }
+    }
+
+    function parse01AsBool(binaryToParse){
+      if (binaryToParse == 1){
+        return true;
+      } else if( binaryToParse == 0){
+        return false;
+      } else {
+        return null;
+      }
+    }
+
+    function parseDate1(date, time){
+      if (time == "" && date != ""){
+        return parseSlashDate(date)
+      } else if (time != ""){
+        return parseReceiveDate(date + "-" + time)
+      } else{
+        return null
+      }
+    }
+
+    function parseDate2(date, time){
+      return parseReceiveDateFullYear(date + "-" + time)
     }
 
     var data = ssv.parse(dataContent, function(d){
-
       return{
-        id:         +d.identification,
-        firstname:  d.vorname,
-        surname:    d.Nachname_zusammen,
-        birthday:   parseBirthday(d.gebdatum),
-        fraction:   d.fraktion,
-        party:      d.partei,
-        religion:   d.religion,
-        randGroup:  d.randomisierungsgruppe,
-        letter:     d.Brief,
-        send:       parseSendDate(+d.Brief, d.EigenesSendedatum, d.EigeneSendezeit, d.Versand1_imputiert)
+        id:                     +d.identification,
+        firstname:              d.vorname,
+        surname:                d.Nachname_zusammen,
+        birthday:               parseBirthday(d.gebdatum),
+        fraction:               d.fraktion,
+        party:                  d.partei,
+        religion:               d.religion,
+        randomGroup:            +d.randomisierungsgruppe,
+        letter:                 parse01AsBool(+d.Brief),
+        lengthOfCommunication:  +d.LenKom,
+        migBackgroundUsed:      parse01AsBool(+d.migh),
+        send:                   parseSendDate(+d.Brief, d.EigenesSendedatum, d.EigeneSendezeit, d.Versand1_imputiert),
+        firstInteractionDate:   parseDate1(d.Datum1, d.Zeit1),
+        secondInteractionDate:  parseDate2(d.Datum2, d.Zeit2)
       };
     });
 
